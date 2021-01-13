55d657b3f5e1287cfac54762f0fe2852ab036360
liufengji <liufengji@aliyun.com>
2021-01-13 23:03:04 +0800

    搭建商家的整体界面

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,10 @@ import Order from '../pages/Order/Order'
 import Profile from '../pages/Profile/Profile'
 import Login from '../pages/Login/Login'
 import UserInfo from '../pages/UserInfo/UserInfo'
+import Shop from '../pages/Shop/Shop'
+import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods'
+import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings'
+import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo'
 
 // 声明使用路由插件
 Vue.use(VueRouter)
@@ -62,6 +66,32 @@ export default new VueRouter({
       path: '/userInfo',
       name: 'UserInfo',
       component: UserInfo
+    },
+    {
+      path: '/shop',
+      name: 'Shop',
+      component: Shop,
+      children: [
+        {
+          path: '/shop/goods',
+          name: 'ShopGoods',
+          component: ShopGoods
+        },
+        {
+          path: '/shop/ratings',
+          name: 'ShopRatings',
+          component: ShopRatings
+        },
+        {
+          path: '/shop/info',
+          name: 'ShopInfo',
+          component: ShopInfo
+        },
+        {
+          path: '',
+          redirect: '/shop/goods'
+        }
+      ]
     }
   ]
 })
