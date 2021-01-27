6c6ab6a14e977e6affa48e6c6cd34250ea2cb921
liufengji <liufengji@aliyun.com>
2021-01-27 19:39:44 +0800

    Search 静态模板

--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -1,15 +1,48 @@
 <template>
   <section class="search">
     <HeaderTop title="搜索"/>
-    <form class="search_form" action="#">
+    <form class="search_form">
       <input type="search" name="search" placeholder="请输入商家或美食名称" class="search_input">
       <input type="submit" name="submit" class="search_submit">
     </form>
+    <section class="list">
+      <ul class="list_container">
+        <li class="list_li">
+          <section class="item_left">
+            <img src="http://elm.cangdu.org/img/169e3971d7c30412.jpg" class="restaurant_img">
+          </section>
+          <section class="item_right">
+            <div class="item_right_text">
+              <p>
+                <span>aaa</span>
+              </p>
+              <p>月售 671 单</p>
+              <p>20 元起送 / 距离1058.2 公里</p>
+            </div>
+          </section>
+        </li>
+        <li class="list_li">
+          <section class="item_left">
+            <img src="http://elm.cangdu.org/img/169add5f3cb29636.jpg" class="restaurant_img">
+          </section>
+          <section class="item_right">
+            <div class="item_right_text">
+              <p>
+                <span>aaa</span>
+              </p>
+              <p>月售 671 单</p>
+              <p>20 元起送 / 距离1058.2 公里</p>
+            </div>
+          </section>
+        </li>
+      </ul>
+    </section>
   </section>
 </template>
 
 <script>
   import HeaderTop from '../../components/HeaderTop/HeaderTop'
+
   export default {
     name: 'Search',
     components: {
@@ -52,4 +85,41 @@
           font-size 16px
           color #fff
           background-color #02a774
+
+    .list
+      .list_container
+        background-color #fff
+
+        .list_li
+          display flex
+          justify-content center
+          padding 10px
+          border-bottom 1px solid $bc
+
+          .item_left
+            margin-right 10px
+
+            .restaurant_img
+              width 50px
+              height 50px
+              display block
+
+          .item_right
+            font-size 12px
+            flex 1
+
+            .item_right_text
+              p
+                line-height 12px
+                margin-bottom 6px
+
+                &:last-child
+                  margin-bottom 0
+
+      .search_none
+        margin 0 auto
+        color #333
+        background-color #fff
+        text-align center
+        margin-top 0.125rem
 </style>
