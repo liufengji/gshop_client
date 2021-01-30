dd4ebe26ba369aa3d1eb8b527c1515e56f7e4d7b
liufengji <liufengji@aliyun.com>
2021-01-30 11:14:38 +0800

    webpack-bundle-analyzer 打包文件分析与优化

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,8 +1,15 @@
 import  Vue from 'vue'
-import moment from 'moment'
+// import moment from 'moment'
+// import {format} from 'date-fns'
+import format from 'date-fns/format'
 
 // todo 自定义过滤器
 // todo npm install moment --save
-Vue.filter('date-format',(value,formatStr='YYYY-MM-DD HH:mm:ss') =>{
-  return moment(value).format(formatStr)
+// Vue.filter('date-format',(value,formatStr='YYYY-MM-DD HH:mm:ss') =>{
+//   return moment(value).format(formatStr)
+// })
+
+// todo npm install date-fns --save
+Vue.filter('date-format', function (value, formatStr) {
+  return format(value, formatStr || 'YYYY-MM-DD HH:mm:ss')
 })
